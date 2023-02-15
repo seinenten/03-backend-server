f3cd03e68282a42ba753c9d34345376ab6004a3f
jorge <20200707@uthh.edu.mx>
2023-02-15 01:23:42 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const express = require('express');
+var cors = require('cors');
+
+const { dbConnection } = require('./database/config.js');
+
+//Crear el servidor de express
+const app = express();
+
+// Configurar CORS
+app.use( cors() );
+
+// Base de datos
+dbConnection();
+
+console.log( process.env );
+
+// Rutas
+app.get( '/', (req, res) => {
+
+    res.json( {
+        ok: true,
+        msg: 'Hola mundo'
+    } )
+
+
+} );
+
+
+app.listen( process.env.PORT , () => {
+    console.log('Servidor corriendo en puerto ' + process.env.PORT);
+} )
