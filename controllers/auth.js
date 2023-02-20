687425565b8cafde7be9a43f68280e94e7444522
jorge <20200707@uthh.edu.mx>
2023-02-20 00:20:47 -0600

    a

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async( req , res = response) => {
 
@@ -45,11 +46,60 @@ const login = async( req , res = response) => {
         })
     }
 
+}
 
+
+const googleSignIn = async( req , res = response) => {
+
+    try {
+
+        const { email, name, picture } = await googleVerify( req.body.token );
+        
+        const usuarioDB =await Usuario.findOne({ email });
+        let usuario;
+
+        if(!usuarioDB){
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        }else {
+            usuario = usuarioDB;
+            usuario.google = true;
+            // usuario.password = '@@@'
+        }
+
+        //Guardar Usuario
+        await usuario.save();
+
+        // Generar el token - JWT
+        const token = await generarJWT( usuario.id );
+        
+        
+        res.json({
+            ok: true,
+            email, name, picture,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'token de Google no es correcto'
+        });
+        
+    }
+    
+    
 
 }
 
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -16,6 +16,14 @@ router.post( '/',
         validarCampos
     ],
     login
+)
+
+router.post( '/google',
+    [
+        check('token', 'El token de google es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    googleSignIn
 )
 
 
