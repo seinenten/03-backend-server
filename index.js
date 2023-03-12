934ec1a33a5f9ca69c405c5cd4d34ffa2bf06efa
jorge <20200707@uthh.edu.mx>
2023-03-12 16:15:01 -0600

    a
    a

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 var cors = require('cors');
@@ -30,7 +31,11 @@ app.use( '/api/medicos', require( './routes/medicos' ) );
 app.use( '/api/todo', require('./routes/busquedas')   );
 app.use( '/api/upload', require('./routes/uploads')   );
 
+//Lo ultimo
 
+app.get( '*', (req,res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+});
 
 app.listen( process.env.PORT , () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
